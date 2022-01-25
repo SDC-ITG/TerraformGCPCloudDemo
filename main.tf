ee5fd5e1689e7809b05185700e10d9e29e2d52da
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-26 08:52:06 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+##----------------------------------------------------------------------
+## Create Instance
+##----------------------------------------------------------------------
+
+# Compute Engine
+resource "google_compute_instance" "instance1" {
+  name         = "terraform-test1"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network    = google_compute_network.vpc.name
+    subnetwork = google_compute_subnetwork.pub_subnet.name
+    access_config {}
+  }
+}
